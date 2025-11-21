fdefbbb2b79d7838de4fa56d310c7f096cb19a99
Danilo Acosta <113366808+Danilo-Acosta5389@users.noreply.github.com>
2025-11-21 16:00:57 +0000

    something

--- commands-front/src/routes/index.tsx
+++ commands-front/src/routes/index.tsx
@@ -1,8 +1,7 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import LandingPage from "../components/LandingPage";
 import { useInView } from "react-intersection-observer";
-import {useEffect} from "react"
-
+import { useEffect } from "react";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -17,36 +16,32 @@ function Index() {
   const { ref, inView } = useInView();
 
   useEffect(() => {
-    console.log("parent in view: " + inView)
-  },[inView])
-  console.log("parent in view: " + inView)
+    console.log("parent in view: " + inView);
+  }, [inView]);
+  console.log("parent in view: " + inView);
   return (
-
-      <div
-        ref={ref}
-        className="flex flex-col items-center justify-center "
-      >
-        <div>
-          <p className="sr-only">placeholder</p>
-        </div>
-        <div className=" flex flex-col items-center justify-center h-[70vh]">
-          <h1 className=" text-4xl font-bold">What are you?</h1>
-          <div className=" mt-8 flex flex-row gap-4">
-            {links.map(({ to, label }) => (
-              <Link
-                to={to}
-                className=" border-2 p-4 rounded-md w-[150px] text-center hover:bg-slate-500 text-2xl"
-                key={to}
-              >
-                {label}
-              </Link>
-            ))}
-          </div>
-        </div>
-        <div>
-          <p className="sr-only">placeholder</p>
-        </div>
-        <LandingPage parentInView={inView} />
+    <div ref={ref} className="flex flex-col items-center justify-center ">
+      <div>
+        <p className="sr-only">placeholder</p>
       </div>
+      <div className=" flex flex-col items-center justify-center h-[70vh]">
+        <h1 className=" text-4xl font-bold">What are you?</h1>
+        <div className=" mt-8 flex flex-row gap-4">
+          {links.map(({ to, label }) => (
+            <Link
+              to={to}
+              className=" border-2 p-4 rounded-md w-[150px] text-center hover:bg-slate-500 text-2xl"
+              key={to}
+            >
+              {label}
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div>
+        <p className="sr-only">placeholder</p>
+      </div>
+      <LandingPage parentInView={inView} />
+    </div>
   );
 }
